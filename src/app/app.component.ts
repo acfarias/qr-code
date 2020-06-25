578efb0f0bd6e693d3d9bb093487f7334fe6d251
adjaircfarias@gmail.com <adjaircfarias@gmail.com>
2020-06-25 00:05:51 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'qr-code-project';
+  title = 'teste-qrcode';
+  elementType = 'url';
+  value = 'Farias-Dev';
+
 }
