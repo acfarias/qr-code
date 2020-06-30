1836095075614b9433d489b25031ef15957fab85
adjaircfarias@gmail.com <adjaircfarias@gmail.com>
2020-06-30 00:38:40 -0300

    nova configuração conforme solicitado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { QRCodeComponent } from 'angularx-qrcode';
+import { debugOutputAstAsTypeScript } from '@angular/compiler';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'teste-qrcode';
-  elementType = 'url';
-  value = 'Farias-Dev';
+export class AppComponent implements OnInit {
+  dataStringQrCode = 'www.google.com; Farias-Dev';
+  linkQrCode = null;
+  @ViewChild("qrcode", { static: false }) qrcode: QRCodeComponent
 
+  ngOnInit(): void {
+    
+  }
+
+  downloadQrCode() {
+    this.linkQrCode = this.qrcode.qrcElement.nativeElement.firstChild.currentSrc;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { NgxQRCodeModule } from '@techiediaries/ngx-qrcode';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QRCodeModule } from 'angularx-qrcode';
 
 @NgModule({
   declarations: [
@@ -12,7 +12,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgxQRCodeModule
+    QRCodeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
